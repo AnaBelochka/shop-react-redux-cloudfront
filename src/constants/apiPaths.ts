6327075c8de9618c2af48bd5e997b3dec0ce8dab
Anastasiya_Sidorenko <anastasiya.sidorenko.ext@pl.abb.com>
2022-07-15 23:28:10 +0300

    task-3: update paths to implemented back-end

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://ujh03qvd0a.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://ujh03qvd0a.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ujh03qvd0a.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://ujh03qvd0a.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://ujh03qvd0a.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
