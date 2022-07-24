67a15019098e31b139b030fec322f6925a0031a6
Anastasiya_Sidorenko <anastasiya.sidorenko.ext@pl.abb.com>
2022-07-24 19:39:46 +0300

    task-4: database

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -46,7 +46,7 @@ export default function Products() {
           <Card className={classes.card}>
             <CardMedia
               className={classes.cardMedia}
-              image={product.imageUrl || ""}
+              image={product.imageurl || ""}
               title="Image title"
             />
             <CardContent className={classes.cardContent}>
