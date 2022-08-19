68b07755a2aac765d8fbde806a0423a4f6972456
anastasiya_sidorenko@epam.com <anastasiya_sidorenko@epam.com>
2022-08-19 10:57:06 +0300

    task-8: containerization

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://ujh03qvd0a.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://xlxnligrcc.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ujh03qvd0a.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://ujh03qvd0a.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://anabelochka-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
